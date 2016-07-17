0ce078eac94baa8531d3f65c3eb766e109318fbf
yang.fan44777@hotmail.com <yang.fan44777@hotmail.com>
2016-07-17 21:09:01 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,29 @@ gulp.task('default', () => {
         .pipe(babel({
             presets: ['stage-3']
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('output'));
 
     gulp.src('./models/**/*.js', {base: './'})
         .pipe(babel({
             presets: ['stage-3']
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('output'));
     gulp.src('./routes/**/*.js', {base: './'})
         .pipe(babel({
             presets: ['stage-3']
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('output'));
     gulp.src('./bin/www', {base: './'})
         .pipe(babel({
             presets: ['stage-3']
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('output'));
+
+    gulp.src('./logs', {base: './'})
+        .pipe(gulp.dest('output'));
+
+
     gulp.src('./node_modules/**', {base: './'})
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('output'));
+
 });
