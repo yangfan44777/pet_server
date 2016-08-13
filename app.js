3c2bf514a18619a903fc79cd3e465897fdc60a6b
yangyuan <1492288361@qq.com>
2016-08-13 20:23:45 +0800

    modify

--- app.js
+++ app.js
@@ -36,13 +36,8 @@ app.use(logger('combined', {stream: accessLogStream}));
 /* 数据库 */
 var dbName = 'petDB'
 
-<<<<<<< HEAD
-// var connectionString = 'mongodb://101.200.150.4/' + dbName
-var connectionString = 'mongodb://localhost/' + dbName
-=======
-//var connectionString = 'mongodb://101.200.150.4/' + dbName
- var connectionString = 'mongodb://127.0.0.1/' + dbName + '?poolSize=100'
->>>>>>> f540bcac70c8b859b404bc87e2b523561f9ab57b
+var connectionString = 'mongodb://101.200.150.4/' + dbName + '?poolSize=100'
+ // var connectionString = 'mongodb://127.0.0.1/' + dbName + '?poolSize=100'
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
--- routes/banner.js
+++ routes/banner.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-var Banner = require('../models/banner');
-var express = require('express');
-var router = express.Router();
-
-router.route('/banner/available').get((req, res) => {
-  Banner.find({ available: 1 }).exec((err, banners) => {
-    if (err) {
-      return res.send(err);
-    }
-    res.json(banners);
-  });
-});
-
-router.route('/banner').post((req, res) => {
-  var reqBody = Object.assign(req.body, { available: 1 });
-
-  const banner = new Banner(reqBody);
-
-  banner.save((err, result) => {
-    if (err) {
-      return res.send(err);
-    }
-    res.send({ message: 'banner created' });
-  });
-});
-
-router.route('/banner/disable/:banner_id').put((req, res) => {
-  Banner.findOne({ _id: req.params.banner_id }, (err, banner) => {
-    if (err) {
-      return res.send(err);
-    }
-    banner.available = 0;
-    banner.save(err => {
-      if (err) {
-        return res.send(err);
-      }
-      res.json({ message: 'Banner is not available now.' });
-    });
-  });
-});
-
-module.exports = router;
-=======
 var Banner = require( '../models/banner' )
 var express = require( 'express' )
 var router = express.Router()
@@ -90,4 +46,3 @@ router.route( '/banner/disable/:banner_id' )
   } )
 
 module.exports = router
->>>>>>> f540bcac70c8b859b404bc87e2b523561f9ab57b
